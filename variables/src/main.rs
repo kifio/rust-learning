7c610c63c5b2f0dcb547512a1640bd56ff566174
Ivan Murashov <iamurashov@yandex.ru>
2024-07-31 20:44:03 +0300

    more chapters

---
+++ variables/src/main.rs
@@ -0,0 +1,11 @@
+const A_S_D_F: u32 = 69 * 22;
+
+fn main() {
+    let mut x = 5;
+    println!("Значение x = {x}");
+
+    x = 6;
+    println!("Значение x = {x}");
+
+    println!("Значение A_S_D_F = {A_S_D_F}");
+}
